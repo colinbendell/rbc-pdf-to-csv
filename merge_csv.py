9bb26cbd3f56f5772392fbdb6b64b9c8de9e301d
Colin Bendell <colin@bendell.ca>
2025-05-10 11:53:15 -0400

    initial commit

---
+++ merge_csv.py
@@ -0,0 +1,154 @@
+import os
+import argparse
+import sys
+import pandas as pd
+from pandas.api.types import is_float_dtype
+from pandas.core.api import DataFrame
+import glob
+from datetime import datetime
+
+
+def canon_date(d: str) -> str:
+  try:
+    return datetime.strptime(d, "%Y/%m/%d").strftime("%Y-%m-%d")
+  except ValueError:
+    pass
+  try:
+    return datetime.strptime(d, "%B %d, %Y").strftime("%Y-%m-%d")
+  except ValueError:
+    pass
+  try:
+    return datetime.strptime(d, "%Y-%m-%d").strftime("%Y-%m-%d")
+  except ValueError:
+    pass
+  raise ValueError("unsupported date format: {}".format(d))
+
+
+def clean_date_column(df: pd.DataFrame, column: str):
+  d = pd.to_datetime(df[column], format="%Y/%m/%d", errors="coerce").min()
+  if pd.isna(d):
+    raise RuntimeError("no valid dates in column {}".format(column))
+  for i in range(len(df)):
+    cur = df.loc[i, column]
+    if pd.isna(cur):
+      df.loc[i, column] = d
+      continue
+    try:
+      d = canon_date(cur)
+    except ValueError:
+      pass
+    df.loc[i, column] = d
+
+def from_csv(csv_file):
+  print("Reading {}".format(csv_file))
+  df = pd.read_csv(csv_file)
+  return df
+
+
+def rbc_chequing(dfs: list[DataFrame], base_dir: str = "."):
+  # print ("Processing chequing account in {}".format(base_dir))
+  # csv_files = glob.glob(f"{base_dir}/**/*.csv", recursive=True)
+  # dfs = [from_csv(f) for f in csv_files]
+
+  # drop any dataframe that includes the column "Transaction Date"
+  dfs = [df for df in dfs if "Transaction Date" not in df.columns]
+  if len(dfs) == 0: return
+
+  df = pd.concat(dfs, ignore_index=True)\
+  # if there are any rows, save the csv
+
+  if len(df) == 0: return
+
+  clean_date_column(df, "Date")
+  df["Withdrawals"] = df["Withdrawals"].astype(float)
+  df["Deposit"] = df["Deposit"].astype(float)
+  df.fillna(value={"Withdrawals": 0.0, "Deposit": 0.0}, inplace=True)
+  df["Description"] = df["Description"].apply(lambda d: d.replace("\n", " "))
+
+  df["Amount"] = (df["Withdrawals"] * -1.0) + df["Deposit"]
+  df.drop(columns=["Withdrawals", "Deposit"], inplace=True)
+  df = df[["Date", "Description", "Amount", "Balance"]]
+  df.sort_values(by=["Date"], ascending=True, kind="mergesort", inplace=True)
+
+  # filter out rows that include "Opening or Closing Balance" in the description
+  df = df[~df["Description"].str.contains("opening balance", na=False, case=False)]
+  df = df[~df["Description"].str.contains("Closing balance", na=False, case=False)]
+
+  if len(df) == 0: return
+
+# remove trailing slashes in the base_dir filename and replace leading "." with empty string
+  output_filename = f"{base_dir.rstrip("/")}_chequing.csv".replace(" ", "_")
+  df.to_csv(output_filename, index=False)
+
+
+def rbc_mastercard(dfs: list[DataFrame], base_dir: str = "."):
+  # Read all CSV files in the directory and subdirectories
+  # and concatenate them into a single dataframe
+
+  # update the glob pattern to include all subdirectories from the base_dir
+
+  # cc_csv_files = glob.glob(f"{base_dir}/**/*.csv", recursive=True)
+  # dfs = [from_csv(f) for f in cc_csv_files]
+
+  # drop any dataframe that don't include the column "Transaction Date"
+  dfs = [df for df in dfs if "Transaction Date" in df.columns]
+  if len(dfs) == 0: return
+
+  df = pd.concat(dfs, ignore_index=True)
+
+  if len(df) == 0: return
+
+  clean_date_column(df, "Transaction Date")
+  clean_date_column(df, "Posting Date")
+  df["Amount"] = df["Amount"].astype(float)
+
+  # Make purchases on credit card negative, credits positive.
+  df["Amount"] = df["Amount"] * -1
+  # Filter out automatic payment to pay off credit card balance.
+  if "Activity Description" in df.columns:
+    df.rename(columns={"Activity Description": "Description"}, inplace=True)
+  df["Description"] = df["Description"].apply(lambda d: d.replace("\n", " "))
+  # filter out rows that include "PAYMENT - THANK YOU" in the description
+  # df = df[~df["Description"].str.contains("PAYMENT - THANK YOU", na=False)]
+  # if len(df) == 0:
+  #   return
+  df.sort_values(by=["Transaction Date"],
+                 ascending=True,
+                 kind="mergesort",
+                 inplace=True)
+  df.to_csv(f"{base_dir.rstrip("/")}_cc.csv", index=False)
+
+def main(args):
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "csv_files",
+        default=glob.glob("**/*.csv", recursive=True),
+        nargs="*",
+        help="input PDF statements (default: **/*.csv)",
+    )
+
+    args = parser.parse_args()
+    csv_files = [f for f in args.csv_files if os.path.isfile(f)]
+    csv_files += [f for d in args.csv_files if os.path.isdir(d) for f in glob.glob(d + "/**/*.csv", recursive=True)]
+
+    if len(csv_files) == 0:
+      print("No PDF files found.")
+      return 1
+
+    # iterate through all the csv files and resolve the nearest common parent directory
+    base_dir = os.path.dirname(csv_files[0])
+    for f in csv_files:
+      while not os.path.commonpath([base_dir, f]) == base_dir:
+        base_dir = os.path.dirname(base_dir)
+        if base_dir == "." or base_dir == "/" or base_dir == "":
+          break
+
+    dfs = [from_csv(f) for f in csv_files]
+
+    rbc_chequing(dfs, base_dir=base_dir)
+    rbc_mastercard(dfs, base_dir=base_dir)
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv))
+
---
+++ pdf2csv.py
@@ -0,0 +1,116 @@
+import argparse
+import json
+import base64
+import requests
+import os
+import sys
+import mysecrets
+import glob
+import re
+import pdfminer.high_level
+
+CREDIT_CARD_PROMPT = """The given pdf is a credit statement. Extract transactions as a CSV with the following columns:
+  - "Transaction Date" with values in the format yyyy/mm/dd. Get the year from the line "Statement from month day to month day, year".
+  - "Posting Date" with values in the format yyyy/mm/dd. Get the year from the line "Statement from month day to month day, year".
+  - "Description" with values inside double quotes. Do not include any `"` or `,` in the value. Include the foreign currency and value in paranthensis with the exchange rate prefixed with ` @ ` if available inside the double quotes.
+  - "Amount" with values formatted as float with two decimal places. Do not put `$` or `,` in the value.
+
+Only output the CSV and no other explanations.
+"""
+
+BANK_ACCOUNT_PROMPT = """The given pdf is a bank statement. Extract transactions as a CSV with the following columns:
+  - "Date" with values in the format yyyy/mm/dd. Get the year from the line "Your opening balance on".
+  - "Description" with values inside double quotes. Do not include any `"` or `,` in the value. Include the foreign currency and value in paranthensis with the exchange rate prefixed with ` @ ` if available inside the double quotes.
+  - "Withdrawals" (also might be called "Cheques & Debits") with values formatted as float with two decimal places. Use 0.00 as the default value. Do not put $ or , in the value.
+  - "Deposit" (also might be called "Deposit & Credits") with values formatted as float with two decimal places. Use 0.00 as the default value. Do not put $ or , in the value.
+  - "Balance" with values formatted as float with two decimal places. Do not put `$` or `,` in the value.
+
+Only output the CSV and no other explanations.
+"""
+
+PDF_CREDITCARD_RE = re.compile(r"creditcard.*(visa|mastercard)", re.IGNORECASE | re.DOTALL | re.MULTILINE)
+
+def pdf_to_csv(prompt: str, file_path: str, force: bool = False) -> bool:
+  """Convert a PDF file to CSV using the Gemini API.
+  Args:
+      prompt (str): The prompt to use for the Gemini API.
+      file_path (str): The path to the PDF file.
+      force (bool): Whether to force overwrite the CSV file if it already exists.
+  Returns:
+      bool: True if the conversion was successful, False if the CSV file already exists.
+  """
+
+  out_csv = file_path.removesuffix(".pdf") + ".csv"
+  if not force and os.path.exists(out_csv):
+    return False
+  model = "gemini-2.0-flash"
+  api_url = "https://generativelanguage.googleapis.com/v1beta/models/{}:generateContent?key={}".format(
+      model, mysecrets.GEMINI_API_KEY)
+  parts = [{"text": prompt}]
+  with open(file_path, "rb") as fp:
+    data = str(base64.b64encode(fp.read()), "utf-8")
+    parts.append({"inlineData": {"mimeType": "application/pdf", "data": data}})
+  body = {
+      "contents": [{
+          "parts": parts,
+      }],
+  }
+  resp = requests.post(api_url, json=body)
+  resp = json.loads(resp.text)
+  texts = []
+  for candidate in resp.get("candidates", []):
+    for part in candidate.get("content", {}).get("parts", []):
+      texts.append(part.get("text", "").strip())
+  text = "\n".join(texts).removeprefix("```csv").removesuffix("```")
+  with open(out_csv, "w") as ofp:
+    ofp.write(text)
+  return True
+
+
+def contains_card_type(file_path: str) -> bool:
+  text = pdfminer.high_level.extract_text(file_path)
+  if PDF_CREDITCARD_RE.search(text):
+    print("Found credit card statement in {}".format(file_path))
+  else:
+    print("Found bank statement in {}".format(file_path))
+  return PDF_CREDITCARD_RE.search(text)
+
+def acct_pdf2csv(files: list[str],  force: bool = False) -> None:
+  for i, pdf in enumerate(files):
+    print("Processing {}/{}: {}.".format(i + 1, len(files), pdf))
+    prompt = CREDIT_CARD_PROMPT if contains_card_type(pdf) else BANK_ACCOUNT_PROMPT
+    if pdf_to_csv(prompt=prompt, file_path=pdf, force=force):
+      print("-- Ok")
+    else:
+      print("-- Skipped")
+
+def main(args):
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "-f",
+        "--force",
+        action="store_true",
+        help="force overwrite of existing CSV files",
+    )
+
+    parser.add_argument(
+        "pdf_files",
+        default=glob.glob("**/*.pdf", recursive=True),
+        nargs="*",
+        help="input PDF statements (default: **/*.pdf)",
+    )
+
+    args = parser.parse_args()
+    force = args.force
+    pdf_files = [f for f in args.pdf_files if os.path.isfile(f)]
+    pdf_files += [f for d in args.pdf_files if os.path.isdir(d) for f in glob.glob(d + "/**/*.pdf", recursive=True)]
+
+    if len(pdf_files) == 0:
+      print("No PDF files found.")
+      return 1
+    pdf_files.sort()
+    acct_pdf2csv(files=pdf_files, force=force)
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv))
